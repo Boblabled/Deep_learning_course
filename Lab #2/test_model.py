df49b57da16288f2d4b25329692addf14ad9bf1b
Maksim <max2002abr@mail.ru>
2025-03-07 14:40:16 +0300

    Отчёт по 2 лабе

--- Lab #2/test_model.py
+++ Lab #2/test_model.py
@@ -3,7 +3,9 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO('runs/detect/train/weights/best.pt')  # 'n' - nano версия (лёгкая)
 
-    results = model('Test/test_image.jpg')
+    image = 'Test/test_image.jpg'
+    image = "rock-paper-scissors-1/valid/images/0098_png.rf.7fb8d3ddf906f1a9b84809a187ac0e4d.jpg"
+    results = model(image)
     for result in results:
         result.show()
         result.save('Test/test_image_detected.jpg')
